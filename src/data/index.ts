a5e375770d2fcbb695b788c775b47f6a6c41d37c
Monil Mehta <156000067+Monil110@users.noreply.github.com>
2025-04-23 04:55:10 +0000

    commit 2

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,3 @@
 import navItems from "@/data/nav-items";
 import projects from "@/data/projects";
-import experiences from "@/data/experiences";
-import studentGuideBlogsMetaData from "@/data/studentGuideBlogsMetaData";
-import recentBlogs from "@/data/recentBlogs";
-
-export { navItems, projects, experiences, studentGuideBlogsMetaData, recentBlogs };
+export { navItems, projects};
